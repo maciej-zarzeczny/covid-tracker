07589e91697851ea860c02acf1d4e6b10062d1c0
maciekz1996 <maciek3609@gmail.com>
2020-10-16 17:54:25 +0200

    Added basic table;

--- src/app/list-page/ListPage.test.js
+++ src/app/list-page/ListPage.test.js
@@ -1,6 +1,39 @@
 import React from "react";
 import { render, screen, wait, fireEvent } from "../../testUtils";
 
+import { ListPage } from "./ListPage";
+import { globalData, countriesData, dayOneDataGermany } from "../../testsMockData";
+
+const initialState = {
+  stats: {
+    status: "succeeded",
+    error: null,
+    countryDataStatus: "succeeded",
+    currentCountry: countriesData[0],
+    data: { Global: globalData, Countries: countriesData },
+    countryData: dayOneDataGermany,
+  },
+};
+
 describe("List Page", () => {
-  it("Should render data on successfull fetch", () => {});
+  it("Should display table with all the data", () => {
+    render(<ListPage />, { initialState: initialState });
+
+    expect(screen.getByText(/Num/i)).toBeInTheDocument();
+    expect(screen.getByText(/Country/i)).toBeInTheDocument();
+    expect(screen.getByText(/Total cases/i)).toBeInTheDocument();
+    expect(screen.getByText(/Total deaths/i)).toBeInTheDocument();
+    expect(screen.getByText(/Total recovered/i)).toBeInTheDocument();
+
+    const tableRows = screen.getAllByRole("row");
+
+    expect(tableRows).toHaveLength(countriesData.length + 1);
+
+    countriesData.forEach((country) => {
+      expect(screen.getByText(country.Country)).toBeInTheDocument();
+      expect(screen.getByText(country.TotalConfirmed)).toBeInTheDocument();
+      expect(screen.getByText(country.TotalDeaths)).toBeInTheDocument();
+      expect(screen.getByText(country.TotalRecovered)).toBeInTheDocument();
+    });
+  });
 });
--- src/features/stats/stats-table/StatsTable.js
+++ src/features/stats/stats-table/StatsTable.js
@@ -1,28 +1,82 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
+import { useSelector } from "react-redux";
 
 import "./StatsTable.css";
+import { selectAllCountries } from "../statsSlice";
 
 export const StatsTable = () => {
+  const countriesData = useSelector(selectAllCountries);
+
+  const [tableData, setTableData] = useState(countriesData);
+  const [sortBy, setSortBy] = useState("cases");
+
+  const renderedRows = tableData.map((country, idx) => (
+    <tr key={idx}>
+      <td>{idx + 1}</td>
+      <td>{country.Country}</td>
+      <td className="stats-table__cell--warning">{country.TotalConfirmed}</td>
+      <td className="stats-table__cell--danger">{country.TotalDeaths}</td>
+      <td className="stats-table__cell--success">{country.TotalRecovered}</td>
+    </tr>
+  ));
+
+  useMemo(() => {
+    switch (sortBy) {
+      case "cases":
+        let sortedByCases = [...countriesData].sort((a, b) => b.TotalConfirmed - a.TotalConfirmed);
+        setTableData(sortedByCases);
+        break;
+
+      case "deaths":
+        let sortedByDeaths = [...countriesData].sort((a, b) => b.TotalDeaths - a.TotalDeaths);
+        setTableData(sortedByDeaths);
+        break;
+
+      case "recovered":
+        let sortedByRecovered = [...countriesData].sort(
+          (a, b) => b.TotalRecovered - a.TotalRecovered
+        );
+        setTableData(sortedByRecovered);
+        break;
+
+      default:
+        break;
+    }
+  }, [sortBy, countriesData]);
+
+  const handleSortedByChange = (value) => {
+    setSortBy(value);
+  };
+
   return (
-    <table className="stats-table">
-      <thead>
-        <tr>
-          <th>Num</th>
-          <th>Country</th>
-          <th>Total cases</th>
-          <th>Total deaths</th>
-          <th>Total recovered</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>United States of America</td>
-          <td>1000</td>
-          <td>500</td>
-          <td>700</td>
-        </tr>
-      </tbody>
-    </table>
+    <div className="stats-table-container card card--big">
+      <table className="stats-table">
+        <thead>
+          <tr>
+            <th>Num</th>
+            <th>Country</th>
+            <th
+              className="stats-table__cell--warning"
+              onClick={() => handleSortedByChange("cases")}
+            >
+              Total cases
+            </th>
+            <th
+              className="stats-table__cell--danger"
+              onClick={() => handleSortedByChange("deaths")}
+            >
+              Total deaths
+            </th>
+            <th
+              className="stats-table__cell--success"
+              onClick={() => handleSortedByChange("recovered")}
+            >
+              Total recovered
+            </th>
+          </tr>
+        </thead>
+        <tbody>{renderedRows}</tbody>
+      </table>
+    </div>
   );
 };
