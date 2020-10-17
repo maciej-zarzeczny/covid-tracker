4d172d808d6096c349df2daa8e2d66addbc7abec
maciekz1996 <maciek3609@gmail.com>
2020-10-17 16:24:55 +0200

    Refactored table in ListPage

--- src/app/list-page/ListPage.test.js
+++ src/app/list-page/ListPage.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen, wait, fireEvent } from "../../testUtils";
+import { render, screen, fireEvent } from "../../testUtils";
 
 import { ListPage } from "./ListPage";
 import { globalData, countriesData, dayOneDataGermany } from "../../testsMockData";
@@ -19,21 +19,28 @@ describe("List Page", () => {
   it("Should display table with all the data", () => {
     render(<ListPage />, { initialState: initialState });
 
+    // Check if there are all table headers
     expect(screen.getByText(/Num/i)).toBeInTheDocument();
     expect(screen.getByText(/Country/i)).toBeInTheDocument();
     expect(screen.getByText(/Total cases/i)).toBeInTheDocument();
+    expect(screen.getByText(/New cases/i)).toBeInTheDocument();
     expect(screen.getByText(/Total deaths/i)).toBeInTheDocument();
+    expect(screen.getByText(/New deaths/i)).toBeInTheDocument();
     expect(screen.getByText(/Total recovered/i)).toBeInTheDocument();
+    expect(screen.getByText(/New recovered/i)).toBeInTheDocument();
 
+    // check if all data are shown
     const tableRows = screen.getAllByRole("row");
-
     expect(tableRows).toHaveLength(countriesData.length + 1);
 
     countriesData.forEach((country) => {
       expect(screen.getByText(country.Country)).toBeInTheDocument();
-      expect(screen.getByText(country.TotalConfirmed)).toBeInTheDocument();
-      expect(screen.getByText(country.TotalDeaths)).toBeInTheDocument();
-      expect(screen.getByText(country.TotalRecovered)).toBeInTheDocument();
+      expect(screen.getByText(`${country.TotalConfirmed}`)).toBeInTheDocument();
+      expect(screen.getByText(`${country.NewConfirmed}`)).toBeInTheDocument();
+      expect(screen.getByText(`${country.TotalDeaths}`)).toBeInTheDocument();
+      expect(screen.getByText(`${country.NewDeaths}`)).toBeInTheDocument();
+      expect(screen.getByText(`${country.TotalRecovered}`)).toBeInTheDocument();
+      expect(screen.getByText(`${country.NewRecovered}`)).toBeInTheDocument();
     });
   });
 });
--- src/features/stats/stats-table/StatsTable.js
+++ src/features/stats/stats-table/StatsTable.js
@@ -1,5 +1,7 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import ReactTooltip from "react-tooltip";
+import classNames from "classnames";
 
 import "./StatsTable.css";
 import { selectAllCountries } from "../statsSlice";
@@ -13,30 +15,100 @@ export const StatsTable = () => {
   const renderedRows = tableData.map((country, idx) => (
     <tr key={idx}>
       <td>{idx + 1}</td>
-      <td>{country.Country}</td>
-      <td className="stats-table__cell--warning">{country.TotalConfirmed}</td>
-      <td className="stats-table__cell--danger">{country.TotalDeaths}</td>
-      <td className="stats-table__cell--success">{country.TotalRecovered}</td>
+      <td
+        data-testid="country-cell"
+        className={classNames("", {
+          "stats-table__cell--bold": sortBy === "country",
+        })}
+      >
+        {country.Country}
+      </td>
+      <td
+        className={classNames("stats-table__cell--warning", {
+          "stats-table__cell--bold": sortBy === "cases",
+        })}
+      >
+        {country.TotalConfirmed}
+      </td>
+      <td
+        className={classNames("stats-table__cell--warning", {
+          "stats-table__cell--bold": sortBy === "new-cases",
+        })}
+      >
+        {country.NewConfirmed}
+      </td>
+      <td
+        className={classNames("stats-table__cell--danger", {
+          "stats-table__cell--bold": sortBy === "deaths",
+        })}
+      >
+        {country.TotalDeaths}
+      </td>
+      <td
+        className={classNames("stats-table__cell--danger", {
+          "stats-table__cell--bold": sortBy === "new-deaths",
+        })}
+      >
+        {country.NewDeaths}
+      </td>
+      <td
+        className={classNames("stats-table__cell--success", {
+          "stats-table__cell--bold": sortBy === "recovered",
+        })}
+      >
+        {country.TotalRecovered}
+      </td>
+      <td
+        className={classNames("stats-table__cell--success", {
+          "stats-table__cell--bold": sortBy === "new-recovered",
+        })}
+      >
+        {country.NewRecovered}
+      </td>
     </tr>
   ));
 
-  useMemo(() => {
+  useEffect(() => {
     switch (sortBy) {
+      case "country":
+        const sortedByCountry = [...countriesData].sort((a, b) => (a.Country > b.Country ? 1 : -1));
+        setTableData(sortedByCountry);
+        break;
+
       case "cases":
-        let sortedByCases = [...countriesData].sort((a, b) => b.TotalConfirmed - a.TotalConfirmed);
+        const sortedByCases = [...countriesData].sort(
+          (a, b) => b.TotalConfirmed - a.TotalConfirmed
+        );
         setTableData(sortedByCases);
         break;
 
+      case "new-cases":
+        const sortedByNewCases = [...countriesData].sort((a, b) => b.NewConfirmed - a.NewConfirmed);
+        setTableData(sortedByNewCases);
+        break;
+
       case "deaths":
-        let sortedByDeaths = [...countriesData].sort((a, b) => b.TotalDeaths - a.TotalDeaths);
+        const sortedByDeaths = [...countriesData].sort((a, b) => b.TotalDeaths - a.TotalDeaths);
         setTableData(sortedByDeaths);
         break;
 
+      case "new-deaths":
+        const sortedByNewDeaths = [...countriesData].sort((a, b) => b.NewDeaths - a.NewDeaths);
+        setTableData(sortedByNewDeaths);
+        break;
+
       case "recovered":
-        let sortedByRecovered = [...countriesData].sort(
+        const sortedByRecovered = [...countriesData].sort(
           (a, b) => b.TotalRecovered - a.TotalRecovered
         );
         setTableData(sortedByRecovered);
+        break;
+
+      case "new-recovered":
+        const sortedByNewRecovered = [...countriesData].sort(
+          (a, b) => b.NewRecovered - a.NewRecovered
+        );
+        setTableData(sortedByNewRecovered);
         break;
 
       default:
@@ -54,29 +126,35 @@ export const StatsTable = () => {
         <thead>
           <tr>
             <th>Num</th>
-            <th>Country</th>
-            <th
-              className="stats-table__cell--warning"
-              onClick={() => handleSortedByChange("cases")}
-            >
+            <th data-tip="Sort by country" onClick={() => handleSortedByChange("country")}>
+              Country
+            </th>
+            <th data-tip="Sort by cases" onClick={() => handleSortedByChange("cases")}>
               Total cases
             </th>
-            <th
-              className="stats-table__cell--danger"
-              onClick={() => handleSortedByChange("deaths")}
-            >
+            <th data-tip="Sort by new cases" onClick={() => handleSortedByChange("new-cases")}>
+              New cases
+            </th>
+            <th data-tip="Sort by deaths" onClick={() => handleSortedByChange("deaths")}>
               Total deaths
             </th>
-            <th
-              className="stats-table__cell--success"
-              onClick={() => handleSortedByChange("recovered")}
-            >
+            <th data-tip="Sort by new deaths" onClick={() => handleSortedByChange("new-deaths")}>
+              New deaths
+            </th>
+            <th data-tip="Sort by recovered" onClick={() => handleSortedByChange("recovered")}>
               Total recovered
+            </th>
+            <th
+              data-tip="Sort by new recovered"
+              onClick={() => handleSortedByChange("new-recovered")}
+            >
+              New recovered
             </th>
           </tr>
         </thead>
         <tbody>{renderedRows}</tbody>
       </table>
+      <ReactTooltip />
     </div>
   );
 };
