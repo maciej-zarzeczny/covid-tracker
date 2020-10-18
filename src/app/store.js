da9e3e391049298eb4d53f56d4cc50daccaa7b75
maciekz1996 <maciek3609@gmail.com>
2020-10-18 16:47:14 +0200

    Added app to github pages

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import statsReducer from "../features/stats/statsSlice";
-import countriesSlice from "../features/countries/countriesSlice";
 
 export default configureStore({
   reducer: {
     stats: statsReducer,
-    countries: countriesSlice,
   },
 });
