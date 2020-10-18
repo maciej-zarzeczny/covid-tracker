bb512ec8a84d504fcbdb25e34de057478dfbf01c
maciekz1996 <maciek3609@gmail.com>
2020-10-18 16:45:21 +0200

    Updates

---
+++ precache-manifest.88817bfdd78a1945ecc23fddee902442.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ae4495db0b8aa1f431ea02cea642ce7",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "512ec07c7a2b1781b490",
+    "url": "/covid-tracker/static/css/main.ca4f90fc.chunk.css"
+  },
+  {
+    "revision": "67efcd4f50b93cc3e959",
+    "url": "/covid-tracker/static/js/2.77af6a62.chunk.js"
+  },
+  {
+    "revision": "254c01d1a3c7d8124adf4c35e46b76e3",
+    "url": "/covid-tracker/static/js/2.77af6a62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "512ec07c7a2b1781b490",
+    "url": "/covid-tracker/static/js/main.af67e6e3.chunk.js"
+  },
+  {
+    "revision": "a2c09be945803fa375fb",
+    "url": "/covid-tracker/static/js/runtime-main.948b6ba1.js"
+  }
+]);
